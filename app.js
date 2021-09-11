8c67d54760c826319f258680182475e79b347432
Shankyy <sankalpt92@gmail.com>
2021-09-11 18:09:31 +0530

    NEW FILE ADDED
    new file added

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("THIS IS JAVASCRIPT")
+
+function windowLoaded(){
+ document.body.style="transition-duration:4s; opacity:1;"
+console.log("five seconds over");
+}
+
+let clsCard = document.getElementsByName
+ 
+
