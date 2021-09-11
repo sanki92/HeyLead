fe8e5157d7284bb6c8e1217f1f3b9ed78e6fe76b
Shankyy <sankalpt92@gmail.com>
2021-09-11 20:09:26 +0530

    Update
    RESPONSIVENESS

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 console.log("THIS IS JAVASCRIPT")
 
+
+let cardCls = document.getElementsByClassName("cards");
+console.log(cardCls);
+
+
 function windowLoaded(){
- document.body.style="transition-duration:4s; opacity:1;"
+    document.body.style="transition-duration:4s; opacity:1;"
 console.log("five seconds over");
 }
-
-let clsCard = document.getElementsByName
- 
-
